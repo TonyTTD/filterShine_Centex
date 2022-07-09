310d691e75c7dddb9b3e445b143455eb100bb6f9
Ubuntu <ubuntu@ip-172-31-88-215.ec2.internal>
2022-07-09 23:12:13 +0000

    Updated address

--- server/index.js
+++ server/index.js
@@ -9,10 +9,16 @@ const app = express();
 app.use(express.json());
 
 const port = process.env.SERVERPORT || 3003;
-
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", `http://44.204.35.57:4000`);
+  res.header(
+    "Access-Control-Allow-Headers"
+  );
+  next();
+});
 app.use(cors({
   origin: [
-    "*", `http://localhost:4000`
+    "*", `http://localhost:4000`, `http://localhost:3000`, `http://44.204.35.57:4000`
   ],
   methods: ["GET", "POST","PUT","DELETE"],
   credentials: false,
